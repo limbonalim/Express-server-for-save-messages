4856c94947a54e1d6058e648a40db7ac44de8147
Zhuravlev Petr <zeterfrost@gmail.com>
2024-01-12 10:22:01 +0600

    added getMessage service for messages route

--- FileBD.ts
+++ FileBD.ts
@@ -1,12 +1,31 @@
 import {promises as fs} from "fs";
+import {Message} from "./types";
 
-const path = './messages/'
+const path = './messages';
+const ignore = '.gitignore';
 
 export const createMessage = async (message: string) => {
   const dateTime = new Date().toISOString();
-  await fs.writeFile(`${path}${dateTime}.txt`, message);
+  await fs.writeFile(`${path}/${dateTime}.txt`, message);
   return {
     message: message,
     datetime: dateTime
   }
+};
+
+export const getMessages = async () => {
+  const files = await fs.readdir(path);
+  const result: Message[] = [];
+
+  for (let i = (files.length - 1); i > 0; i--) {
+    if (files[i] !== ignore && result.length < 5) {
+      const message = await fs.readFile(`${path}/${files[i]}`);
+
+      result.push({
+        message: message.toString(),
+        datetime: files[i].slice(0, -4)
+      });
+    }
+  }
+  return result;
 };
--- routers/messages.ts
+++ routers/messages.ts
@@ -1,11 +1,12 @@
 import {Router} from "express";
-import {createMessage} from "../FileBD";
+import {createMessage, getMessages} from "../FileBD";
 import {Message} from "../types";
 
 const messagesRouter = Router();
 
-messagesRouter.get('/', (req, res) => {
-  res.send('Get all messages');
+messagesRouter.get('/', async (req, res) => {
+  const answer: Message[] = await getMessages();
+  res.send(answer);
 });
 
 messagesRouter.post('/', async (req, res) => {
