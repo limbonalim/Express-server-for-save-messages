fd180b3503c0d2a07871f66279530aa051e19781
Zhuravlev Petr <zeterfrost@gmail.com>
2024-01-12 11:08:16 +0600

    added home page route in index.ts

--- index.ts
+++ index.ts
@@ -3,9 +3,14 @@ import messagesRouter from './routers/messages';
 
 const app = express();
 const port = 8000;
+const path = 'http://localhost:8000/messages';
 
 app.use(express.json());
 app.use('/messages', messagesRouter);
+
+app.get('/', (req, res) => {
+	res.send(`<div>You can send a post request your message to <b>${path}</b>, Or you can send a get request</div>`);
+});
 
 app.listen(port, () => {
 	console.log(`Server started on ${port} port!`);
